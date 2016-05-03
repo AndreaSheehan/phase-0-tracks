d0dafd157980afa289b85d1aaa4bb084aa5c56c9
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-05-03 15:56:02 -0400

    Reorder and cinditions now met and working.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -24,48 +24,52 @@ current_year = 2016.to_i\
 #If employee age == calculation of age from birth year && they order garlic bread || sign up for insurance.
 #Printed result: NOT a vampire
 
-if 
-	 name = "Drake Cula" 
-		puts "Definitely a vampire!"
+secret_name = "Drake Cula" 
+secret_name2 = "Tu Fang"
 
-	elsif name = "Tu Fang"
-		puts "Definitely a vampire!"
 
-	elsif age == current_year - birth_year && lunch == "yes" || health_plan == "yes"
+if
+
+	name == secret_name || name == secret_name2
+	puts "Definitely a vampire!"
+
+elsif
+	  age == current_year - birth_year && (lunch == "yes" || health_plan == "yes")
 
 	puts "Candidate is probably NOT a vampire"
+
+
+#If Employee age != birth year calculation, && hates garlic bread  || does not want insurance
+#Printed result:  Probable VAMPIRE
+
+elsif
+	age != current_year - birth_year && (lunch == "no" && health_plan == "no")
+		puts "Candidate is almost certainly a vampire"
+
+
+elsif
+	age != current_year - birth_year && (lunch == "no" || health_plan == "no" )
+	puts "Candidate is probably a vampire"
 
 
 #If Employee age != birth year calculation, && hates garlic bread && does not want insurance
 #Printed result:  Almost certainly VAMPIRE
 
-	elsif
-		age != current_year - birth_year && lunch == "no" && health_plan == "no"
-		puts "Candidate is almost certainly a vampire"
 
 
-#If Employee age != birth year calculation, && hates garlic bread  || does not want insurance
-#Printed result:  Probable VAMPIRE
-	elsif
-		age != current_year - birth_year && (lunch == "no" || health_plan == "yes")
-		puts "Candidate is probably a vampire"
-
-	elsif 
-		age != current_year - birth_year && (lunch == "yes" || health_plan == "no")
-		puts "Candidate is probably a vampire"
+	
 
 
-
-	#else
-		#puts "Resuls Inconclusive"
-
+else
+	puts "Resuls Inconclusive"
+end
 	
 #
 #Elsif employee puts DRAKE CULA or TU FANG 
 #Printed result is: Definitely a vampire 
 #
 #Else result : Inconclusive
-end
+
 #END program and puts Results inconclusive
 #
 #
