642cc0370aaa16273c6165632a93c488165bc491
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-05-03 16:10:04 -0400

    Add employee count

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,64 +4,68 @@
 # Offer candidate garlic bread
 # Offer enrollment in health insurance plan
 
-puts "Welcome! What is your name?"
-name = gets.chomp
+puts "How many new employees are being processed?"
+employees = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp.to_i
 
-puts "Please enter your year of birth?"
-birth_year = gets.chomp.to_i
+until employees == 0
+	employees -=1
 
-puts "Today's lunch special is garlic bread.  Shall we order some for you? (yes/no)"
-lunch = gets.chomp
+	puts "Welcome! What is your name?"
+	name = gets.chomp
 
-puts "Enrollment in our health plan is optional.  Shall we enroll you? (yes/no)"
-health_plan = gets.chomp
+	puts "How old are you?"
+	age = gets.chomp.to_i
 
-current_year = 2016.to_i\
+	puts "Please enter your year of birth?"
+	birth_year = gets.chomp.to_i
+
+	puts "Today's lunch special is garlic bread.  Shall we order some for you? (yes/no)"
+	lunch = gets.chomp
+
+	puts "Enrollment in our health plan is optional.  Shall we enroll you? (yes/no)"
+	health_plan = gets.chomp
+
+	current_year = 2016.to_i\
 #RESULTS:
 #If employee age == calculation of age from birth year && they order garlic bread || sign up for insurance.
 #Printed result: NOT a vampire
 
-secret_name = "Drake Cula" 
-secret_name2 = "Tu Fang"
+	secret_name = "Drake Cula" 
+	secret_name2 = "Tu Fang"
 
 
-if
+	if
 
-	name == secret_name || name == secret_name2
-	puts "Definitely a vampire!"
+		name == secret_name || name == secret_name2
+		puts "Definitely a vampire!"
 
-elsif
-	  age == current_year - birth_year && (lunch == "yes" || health_plan == "yes")
+	elsif
+	  	age == current_year - birth_year && (lunch == "yes" || health_plan == "yes")
 
-	puts "Candidate is probably NOT a vampire"
+		puts "Candidate is probably NOT a vampire"
 
 
 #If Employee age != birth year calculation, && hates garlic bread  || does not want insurance
 #Printed result:  Probable VAMPIRE
 
-elsif
-	age != current_year - birth_year && (lunch == "no" && health_plan == "no")
+	elsif
+		age != current_year - birth_year && (lunch == "no" && health_plan == "no")
 		puts "Candidate is almost certainly a vampire"
 
 
-elsif
-	age != current_year - birth_year && (lunch == "no" || health_plan == "no" )
-	puts "Candidate is probably a vampire"
+	elsif
+		age != current_year - birth_year && (lunch == "no" || health_plan == "no" )
+		puts "Candidate is probably a vampire"
 
 
 #If Employee age != birth year calculation, && hates garlic bread && does not want insurance
 #Printed result:  Almost certainly VAMPIRE
-
-
-
 	
 
-
-else
-	puts "Resuls Inconclusive"
+	else
+		puts "Results Inconclusive"
+	end
 end
 	
 #
