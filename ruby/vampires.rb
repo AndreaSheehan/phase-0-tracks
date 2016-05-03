46ea0dcc2462825e4401f23e90b6b7bb3619fe25
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-05-03 13:50:41 -0400

    Create initial script for vampire detection.

---
+++ ruby/vampires.rb
@@ -0,0 +1,20 @@
+#Vampire Detection program
+#Get applicant name for screening agains WIB
+#Get applicant age and year of birth
+# Offer candidate garlic bread
+# Offer enrollment in health insurance plan
+
+puts "Welcome! What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "Please enter your year of birth?"
+birth_year = gets.chomp
+
+puts "Today's lunch special is garlic bread.  Shall we order some for you? (y/n)"
+lunch = gets.chomp
+
+puts "Enrollment in our health plan is optional.  Shall we enroll you? (y/n)"
+health_plan = gets.chomp
