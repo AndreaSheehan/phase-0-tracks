769ae238ea8b4bd82a755517c23914e2fd079f75
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-05-13 12:20:28 -0400

    Add nested code from repli.it exxperimentation for assignment

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,43 @@
+kitchen = {
+	drawer_stack_one: {
+		drawer_one: 'utensils',
+		drawer_two: 'dishes',
+		drawer_three: 'glassware'
+	},
+
+	drawer_stack_two: {
+		drawer_one: 'oven mitts',
+		drawer_two: 'mixing bowls',
+		drawer_three: 'bakeware'
+	},
+	
+	pantry:[['canned goods', 'bouillon'],['cereals', 'crackers', 'energy bars'], ['pasta', 'rice']
+	]
+
+}
+
+
+# test for first piece of code
+#kitchen[:drawer_stack_one][:drawer_one] => "utensils"
+
+
+ # test for 2nd piece of code 
+ # kitchen[:drawer_stack_two][:drawer_two] => "mixing bowls"
+ 
+
+# test for 3rd piece of code  
+# kitchen[:pantry][1][2] => 'energy bars'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
