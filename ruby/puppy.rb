351d166ef79b41d7d3aa5c4ac18a69896f387149
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-05-16 14:22:03 -0400

    Initial commit release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,22 @@
+#class : Puppy
+#characteristics:
+#friendly: yes
+#tail: yes
+#legs: 4
+#ears: [floppy, pointed]
+#breed: varied
+#owner: human
+#intelligent: yes
+#size; [xsmall, small, medium, large ]
+#
+#Behaviors:
+#jump
+#run
+#sleep
+#roll-over
+#bark
+#lick
+#eat
+#link
+#poop
+#cuddle
