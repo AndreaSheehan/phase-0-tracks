d1bca9bae0044683b59005f3aca82286e233fb26
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-05-16 18:02:59 -0400

    Update with formatting

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,50 +1,82 @@
 class Puppy
-
-  def fetch(toy="ball o monkeys")
+	
+	 def fetch(toy="ball o monkeys")
     	puts "I brought back the #{toy}!"
-    toy
-  end
+    	toy
+  	end
   
-
-end
-
-chloe = Puppy.new
-chloe.fetch("doll")
-
-class Puppy
 	def speak(n)
 		n.times { puts "woof"}
  	end
-end
 
 
-class Puppy
 	def roll_over
 		 puts "*rolls over*"
  	end
-end
-	
-chloe.speak(3)
-
-chloe.roll_over
 
 
-class Puppy
 	def dog_years(n)
-	dog_age = n * 7
+		dog_age = n * 7
 	end	
-end
 
-chloe.dog_years(4)
-
-class Puppy
 	def cold_night(temp)
 		if temp < 32
 			puts "*cuddle cuddle*"
 		else puts "*pant pant*"
 		end
 	end
+
+
+	def initialize
+		puts "Initializing new puppy instance"
+	end	
 end
+
+
+chloe = Puppy.new
+
+chloe.fetch("doll")
+chloe.speak(3)
+chloe.roll_over
+chloe.dog_years(4)
 chloe.cold_night(50)
+
+# Ninja class
+# characteristics
+# clothes:[black, blue, red, green]
+# invisibility: yes
+# gender: [male, female]
+# age: varied
+# 
+# Behaviors
+# juggles
+# flips
+# throws_stars
+# disappears
+# saves_humankind
+# 
+class Ninja
+
+	def initialize
+		puts "Ninja initialized"
+	end
+
+	def juggles 
+		puts "*items flip madly through air in succession*"
+	end
+
+	def saves_humankind
+		puts "Hurray, Hurray! We are saved!"
+	end
+	
+end
+
+green_ninja = Ninja.new
+
+green_ninja.juggles
+
++ []
+
+
 
 
