6eda7ec7c82c7fa870784076b70cbed10f1d22c5
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-05-16 15:19:52 -0400

    Do the thing: speak, rollover

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,30 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
+  def fetch(toy="ball o monkeys")
+    	puts "I brought back the #{toy}!"
     toy
   end
+  
 
 end
+
+chloe = Puppy.new
+chloe.fetch("doll")
+
+class Puppy
+	def speak(n)
+		n.times { puts "woof"}
+ 	end
+end
+
+
+class Puppy
+	def roll_over
+		 puts "*rolls over*"
+ 	end
+end
+	
+chloe.speak(3)
+
+chloe.roll_over
 
