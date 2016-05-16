ccda443c93a828d2fac2c0f7e74f910d851cec25
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-05-16 15:38:51 -0400

    Add dog years, new trick

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -28,3 +28,23 @@ chloe.speak(3)
 
 chloe.roll_over
 
+
+class Puppy
+	def dog_years(n)
+	dog_age = n * 7
+	end	
+end
+
+chloe.dog_years(4)
+
+class Puppy
+	def cold_night(temp)
+		if temp < 32
+			puts "*cuddle cuddle*"
+		else puts "*pant pant*"
+		end
+	end
+end
+chloe.cold_night(50)
+
+
