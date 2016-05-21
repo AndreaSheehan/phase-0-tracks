c2d2a6797f7cf6a54cd72e7e85f175c44ebe9f95
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-05-21 17:57:48 -0400

    Add release 1 diverse initializations

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,19 @@
 class Santa
 
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance"
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking =  ["Rudolph", "Dasher", "Dancer", "Prancer", 
+			"Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+
+	end
+
 	def speak(n)
 		puts "Ho, ho, ho! Haaaappy holidays!"
 
 	end
-
 
 
 	def eat_milk_and_cookies(cookie)
@@ -13,12 +22,37 @@ class Santa
 
 	end
 
-
-	def initialize
-		puts "Initializing Santa instance"
-
-	end
 end
 
-scott = Santa.new
-scott.speak(1)
+
+
+
+santas = []
+facebook_gender_list = [
+	"Agender", "Androgyne", "Androgynous", "Bigender", "Cis", "Cis Female","Cis Male", "Cis Man", "Cis Woman", "Cisgender", "Cisgender Female", "Cisgender Male", "Cisgender Man", "Cisgender Woman", "Female to Male", "FTM", "Gender Fluid", "Gender Nonconforming", "Gender Questioning", "Gender #Variant", "Genderqueer", "Intersex", "Male to Female", "MTF", "Neither", "Neutrois", "Non#-binary", "Other", "Pangender", "Trans","Trans Female", "Trans Male", "Trans Man", "Trans Person", "Trans Woman", "Trans*", "Trans* Female","Trans* Male", "Trans* Man", "Trans* Person", "Trans* Woman", "Transfeminine", "Transgender","Transgender Female", "Transgender Male", "Transgender Man", "Transgender Person", "Transgender Woman", "Transmasculine", "Transsexual", "Transsexual Female", "Transsexual Male", "Transsexual Man", "Transsexual Person", "Transsexual Woman", "Two-spirit"
+	]
+   
+ethnicity_list = [
+	"Pashtun","Tajik","Arab","Hazara","Uzbek","Guar","Albanian","Greek","Asian","Andorran","Spanish","Portuguese", "Black","White","Hispanic","Armenian","Dutch","Colombian","Australian", "Bahraini","Bengali","Belarusian","Russian", "Ngalop","Mestizo","Bosni", "Malay","Chinese","Turkish", "Burman","Shan","Creole","Vietnamese","Croat","Egyptian", "Fang", "Estonian", "Scandinavian","Georgian","Finn","German","Amhara","Somali","Akan","Cafra","Makoa", "Zing","Karen","Tswana", "Undeclared"
+	]   
+   
+ 
+  facebook_gender_list.length.times do |i|
+  	santas << Santa.new(facebook_gender_list[i], ethnicity_list[i])
+  end
+
+
+#Update your Santa class with the following attributes:
+
+#gender, which will be a string passed in on initialization
+#ethnicity, which will be a string passed in on initialization
+#reindeer_ranking, an array of reindeer from most preferred to least preferred. This is not passed in on initialization; it simply gets a default value of ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+#age, which is not passed in on initialization and defaults to 0
+#santas = []
+#santas << Santa.new("agender", "black")
+#santas << Santa.new("female", "Latino")
+#santas << Santa.new("bigender", "white")
+#santas << Santa.new("male", "Japanese")
+#santas << Santa.new("female", "prefer not to say")
+#santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+#santas << Santa.new("N/A", "N/A")
