d765ab16c92788b1d6920fb9499c06922ec990d6
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-06-02 23:11:36 -0400

    Add first release

--- js/algos.js
+++ js/algos.js
@@ -27,21 +27,88 @@
 //var creatures = ["vampire", "zombie", "werewolf"]
 //Oops. Save that for driver code; this is a FUNCTION my words should be generic but descriptive
 
-//var array = ['string', 'longer string', "longest string"]
 
-function longestString {
-	var longest = ""
 
-	for (var i = 0; i < array.length; i++) {
-		if (array[i].length > longest.length) {
-		longest = array[i];
-		}
-	}
+function longestString(array) {
+  var longest = "";
 
+
+  for (var i = 0; i < array.length; i++) {
+  
+       if (array[i].length > longest.length) {
+         longest = array[i]
+       }
+      
+   }
+      return longest
 }
 
-return longest;
 
+var answer = (longestString(["vampire", "zombie", "werewolf"]))
 
-answer = longest(["vampire", "zombie"]);
 console.log(answer)
+
+
+
+
+// function longestString(array) {
+//   var longest = "";
+
+
+//  notes from office hours:
+// for (var i in array) {
+//        if (array[i].length > longest.length) {
+//          longest = array[i]
+//        }
+      
+//    }
+//       return longest
+// }
+
+
+// var answer = (longestString(["vampire", "zombie", "werewolf"]))
+
+// console.log(answer)
+
+//Describe:
+//function Similar 
+//compare
+
+//expect
+//var sally = { hairColor: 'brown', name: 'Sally', age: 35 }
+//var bobby = { name: 'Bobby', age: 24, hairColor: 'brown' }
+// => true
+
+
+
+
+the purpose of this thing is to compare two objects
+input: 2 objects
+output: true or false
+
+look at first key of first object
+  compare to first key of second object
+  are they the same?
+  if so, compare the values
+    are they the same?
+    if so, return true
+  if not compare to second key of second object and so on
+
+
+...if i get through all possible comparisons and have found nothing, return false
+
+// function Person(name, age, hairColor) {
+//   this.name = name;
+//   this.age = age;
+//   this.hairColor = hairColor;
+  
+//   this.getOlder = function() {
+//     this.age += 1
+//   }
+// }
+
+// var cindy = new Person('Cindy', 56, 'blonde')
+
+// console.log(cindy.age)
+// cindy.getOlder()
+// console.log(cindy.age)
