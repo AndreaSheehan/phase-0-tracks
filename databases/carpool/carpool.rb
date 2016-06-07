0fa4628a03e8004a4b7c2f2fd3f15577dc115caa
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-06-07 10:47:20 -0400

    Initial commit with psuedocode

---
+++ databases/carpool/carpool.rb
@@ -0,0 +1,11 @@
+#make a program to 
+#accept input of children's names, age, parent contact info (name and number)
+#the above will go into a sql table of childrens names
+#accept input for activities that children attend (name of activity and address)
+#the above will be captured in an acitivities table
+#accept input of carpool drivers:  driver name, foreign id to link to children's table. foreign id to link to acitivities table. 
+#driver will actually be a parent of a child in the childrens table so the id of parent will match as a field 
+#from id of child. (hmmm. poss need to consider parent table...  but i think this will work)
+# challenge:  if time consider adding times to the table so that availability matches can be 
+# set to pair a parent with a possible time to drive.  this will make this program 
+# actually useful, 
