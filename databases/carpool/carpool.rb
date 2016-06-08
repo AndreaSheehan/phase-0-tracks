2e1380448af3bdc8a895a6d4f112b4e58d3f0276
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-06-08 00:53:26 -0400

    Add driver code for hours

--- databases/carpool/carpool.rb
+++ databases/carpool/carpool.rb
@@ -57,7 +57,7 @@ end
 #
 #
 #db = SQLite3::Database.new("children.db")
-db = SQLite3::Database.new("children.db")
+
 
 create_table_cmd = <<-SQL
 CREATE TABLE IF NOT EXISTS activities(
@@ -69,18 +69,48 @@ CREATE TABLE IF NOT EXISTS activities(
 )
 SQL
 
-db.execute(create_table_cmd)
-db.execute("INSERT INTO activities (activity, location, weekday) VALUES ('Soccer', 'Avenel Park','Mondays')")
-db.execute("INSERT INTO activities (activity, location, weekday) VALUES ('Karate', 'Potomac Center','Wednesdays')")
-db.execute("INSERT INTO activities (activity, location, weekday) VALUES ('Gymnastics', 'Dynamite Rockville','Fridays')")
+# table created:
+# db.execute(create_table_cmd)
+# db.execute("INSERT INTO activities (activity, location, weekday) VALUES ('Soccer', 'Avenel Park','Mondays')")
+# db.execute("INSERT INTO activities (activity, location, weekday) VALUES ('Karate', 'Potomac Center','Wednesdays')")
+# db.execute("INSERT INTO activities (activity, location, weekday) VALUES ('Gymnastics', 'Dynamite Rockville','Fridays')")
 # # def create_activity(db, activity, location, weekday)
 # # db.execute("INSERT INTO activities (activity, location, weekday) 
 # 	 	VALUES (?, ?, ?)", [activity, location, weekday])
 # end
 # #
 #
+create_table_cmd = <<-SQL
+CREATE TABLE IF NOT EXISTS drivers(
+	id INTEGER PRIMARY KEY,
+	parent_driver VARCHAR(255),
+	child_id INT,
+	activity_id INT,
+	FOREIGN KEY(child_id) REFERENCES children(id),
+	FOREIGN KEY(activity_id) REFERENCES activities(id)
+	
+)
+SQL
+# Populate table:
+# db.execute(create_table_cmd)
+
+# db.execute("INSERT INTO drivers (parent_driver, child_id, activity_id) VALUES ('Martin Wolf', 1, 2 )")
+# db.execute("INSERT INTO drivers (parent_driver, child_id, activity_id) VALUES ('Garett Dubuque', 9, 3 )")
+# db.execute("INSERT INTO drivers (parent_driver, child_id, activity_id) VALUES ('Kolby Larkin', 11, 1 )")
+
 #
+#method will retrieve the activity and match the code for the activity to deliver the driver  
+#def retrieve_driver(db,activity)
+#driver = db.execute("SELECT * FROM drivers JOIN activities ON activity_id = activities.id)
+# driver = db.execute("SELECT * FROM drivers, activities WHERE activity_id = activities.id WHERE activity = #{response}")
+def get_driver (db, response)
+	driver = db.execute("SELECT * FROM drivers JOIN activities ON activity_id = activities.id) WHERE activity ='" + response)
+	puts driver
+end
 #
-#
-#
-#
+puts "Welcome to the carpool info portal! Is your 
+child attending Karate, Gymnastics, or Soccer?"
+response = gets.chomp.downcase.capitalize
+
+get_driver(db, response)
+#response = gets.chomp"
