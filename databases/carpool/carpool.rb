08c33e94f9b06ae447faf514d5c535523605b79b
Andrea Sheehan <andreasheehan@dbc-adreasheehan.home>
2016-06-07 22:50:07 -0400

    Add create child table)

--- databases/carpool/carpool.rb
+++ databases/carpool/carpool.rb
@@ -25,16 +25,59 @@
 # add drive to activity or drive from acitivity
 # 
 # create SQLite3 database.  Want array of all the data so accessible by  index.
+
+require 'sqlite3'
+require 'faker'
+
 db = SQLite3::Database.new("children.db")
 
 create_table_cmd = <<-SQL
+CREATE TABLE IF NOT EXISTS children(
 	id INTEGER PRIMARY KEY,
 	first_name VARCHAR(255),
 	last_name VARCHAR(255),
-	age INTEGER
-	parent_name VARCHAR(255)
-	phone_number INTEGER
-
+	age INT,
+	parent_name VARCHAR(255),
+	phone_number INT
+)
 SQL
 
 db.execute(create_table_cmd)
+
+def create_child(db, first_name, last_name, age, parent_name, phone_number)
+	 db.execute("INSERT INTO children (first_name, last_name, age, parent_name, phone_number) 
+	 	VALUES (?, ?, ?, ?, ?)", [first_name, last_name, age, parent_name, phone_number])
+end
+#code below generated 15 children
+#5.times do 
+#	create_child(db, Faker::Name.first_name, Faker::Name.last_name, 9, Faker::Name.name,
+#	Faker::PhoneNumber.cell_phone)
+
+#end
+#
+#
+#db = SQLite3::Database.new("children.db")
+
+create_table_cmd = <<-SQL
+CREATE TABLE IF NOT EXISTS activities(
+	id INTEGER PRIMARY KEY,
+	activity VARCHAR(255),
+	location VARCHAR(255),
+	weekday VARCHAR(255),
+	
+)
+SQL
+
+db.execute(create_table_cmd)
+
+def create_activity(db, activity, location, weekday)
+	 db.execute("INSERT INTO activities (activity, location, weekday) 
+	 	VALUES (?, ?, ?)", [activity, location, weekday])
+end
+#
+#
+#
+#
+#
+#
+#
